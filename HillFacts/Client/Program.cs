9b0d9208bdb4e17a459b5668054e07405ff50037
Sunny Kolattukudy <kolatts@gmail.com>
2020-11-16 21:05:39 -0500

    Adding twitter feed + generic onload JS interop "jsEmbed"

--- HillFacts/Client/Program.cs
+++ HillFacts/Client/Program.cs
@@ -12,8 +12,6 @@ using Blazor.Analytics;
 using HfTimeline;
 using HfTimeline.Services;
 using JSEmbed.Services;
-using HillFacts.Client.Services;
-using HillFacts.Client.ViewModels;
 
 namespace HillFacts.Client
 {
@@ -22,17 +20,13 @@ namespace HillFacts.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
-            builder.RootComponents.Add<App>("#app");
-            builder.Services.AddScoped(sp => http);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBootstrapCss();
             builder.Services.AddGoogleAnalytics("G-TN4Z59Z2QV");
             builder.Services.AddBlazorToastr();
             builder.Services.AddJsEmbed();
-            builder.Services.AddSingleton<IAppCacheService>(new AppCacheService(http));
-
-            builder.Services.AddScoped<IMembersViewModel, MembersViewModel>();
-            builder.Services.AddScoped<IMemberDetailViewModel, MemberDetailViewModel>();
             await builder.Build().RunAsync();
         }
     }
