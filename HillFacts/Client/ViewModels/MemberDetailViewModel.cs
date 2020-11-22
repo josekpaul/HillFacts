618156422b64fa5adb77d744f9247b6d86d9b5c8
josekpaul <jose.k.paul@gmail.com>
2020-11-22 15:48:04 -0500

    Mvvm implementation (#10)
    * Added AppCacheService to use in place of AppState component. This will make the viewmodel dependency cleaner.
    
    * Moved Members list to MVVM
    
    * Converted MemberDetail page to MVVM
    
    * ..

---
+++ HillFacts/Client/ViewModels/MemberDetailViewModel.cs
@@ -0,0 +1,30 @@
+﻿using HillFacts.Client.Services;
+using ProPublicaCongressAPI.Contracts;
+using System.Threading.Tasks;
+
+namespace HillFacts.Client.ViewModels
+{
+    public class MemberDetailViewModel : BaseViewModel, IMemberDetailViewModel
+    {
+        string memberId;
+        public string MemberId { get { return memberId; } set { SetValue<string>(ref memberId, value); } }
+        Member member;
+        public Member Member { get { return member; } set { SetValue<Member>(ref member, value); } }
+        RecentBillsByMemberContainer recentSponsoredBills;
+        public RecentBillsByMemberContainer RecentSponsoredBills
+        { get { return recentSponsoredBills; } set { SetValue<RecentBillsByMemberContainer>(ref recentSponsoredBills, value); } }
+
+        IAppCacheService cacheService;
+
+        public MemberDetailViewModel(IAppCacheService cacheSvc)
+        {
+            cacheService = cacheSvc;
+        }
+
+        public async Task GetMemberDetail()
+        {
+            Member = await cacheService.CallCacheableServerMethod<Member>($"/api/Propublica/GetMemberDetail?id={MemberId}");
+            RecentSponsoredBills = await cacheService.CallCacheableServerMethod<RecentBillsByMemberContainer>($"/api/Propublica/GetRecentBillsByMember?id={MemberId}"); ;
+        }
+    }
+}
