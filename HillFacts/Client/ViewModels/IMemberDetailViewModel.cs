4397a419710dd6ac32ce593313285ec9ded77150
josekpaul <jose.k.paul@gmail.com>
2020-11-22 15:48:04 -0500

    Mvvm implementation (#10)
    * Added AppCacheService to use in place of AppState component. This will make the viewmodel dependency cleaner.
    
    * Moved Members list to MVVM
    
    * Converted MemberDetail page to MVVM
    
    * ..

---
+++ HillFacts/Client/ViewModels/IMemberDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ProPublicaCongressAPI.Contracts;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace HillFacts.Client.ViewModels
+{
+    public interface IMemberDetailViewModel
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+
+        string MemberId { get; set; }
+        Member Member { get; set; }
+        RecentBillsByMemberContainer RecentSponsoredBills { get; set; }
+
+        Task GetMemberDetail();
+
+    }
+}
