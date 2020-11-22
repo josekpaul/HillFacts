61479d50841d09ee4215bbbaa8b175a11e7dd2a9
josekpaul <jose.k.paul@gmail.com>
2020-11-22 17:56:36 -0500

    Campaign finance api client (#11)
    * Added AppCacheService to use in place of AppState component. This will make the viewmodel dependency cleaner.
    
    * Moved Members list to MVVM
    
    * Converted MemberDetail page to MVVM
    
    * ..
    
    * Added initial client stuff for campaign finance api

---
+++ Propublica.CampaignFinance.Api.Contracts/IndependentExpenditurePerCandidate.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Propublica.CampaignFinance.Api.Contracts
+{
+    // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+    public class IndependentExpenditurePerCandidateResult
+    {
+        [JsonPropertyName("fec_committee")]
+        public string FecCommittee { get; set; }
+
+        [JsonPropertyName("fec_committee_name")]
+        public string FecCommitteeName { get; set; }
+
+        [JsonPropertyName("candidate_name")]
+        public string CandidateName { get; set; }
+
+        [JsonPropertyName("amount")]
+        public double Amount { get; set; }
+
+        [JsonPropertyName("office")]
+        public string Office { get; set; }
+
+        [JsonPropertyName("state")]
+        public object State { get; set; }
+
+        [JsonPropertyName("district")]
+        public object District { get; set; }
+
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("purpose")]
+        public string Purpose { get; set; }
+
+        [JsonPropertyName("payee")]
+        public string Payee { get; set; }
+
+        [JsonPropertyName("date_received")]
+        public string DateReceived { get; set; }
+
+        [JsonPropertyName("fec_uri")]
+        public object FecUri { get; set; }
+
+        [JsonPropertyName("amendment")]
+        public object Amendment { get; set; }
+
+        [JsonPropertyName("support_or_oppose")]
+        public string SupportOrOppose { get; set; }
+
+        [JsonPropertyName("transaction_id")]
+        public string TransactionId { get; set; }
+
+        [JsonPropertyName("unique_id")]
+        public string UniqueId { get; set; }
+
+        [JsonPropertyName("filing_id")]
+        public int FilingId { get; set; }
+
+        [JsonPropertyName("amended_from")]
+        public object AmendedFrom { get; set; }
+
+        [JsonPropertyName("dissemination_date")]
+        public string DisseminationDate { get; set; }
+
+        [JsonPropertyName("form_type")]
+        public string FormType { get; set; }
+    }
+
+    public class IndependentExpenditurePerCandidateResponse
+    {
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("copyright")]
+        public string Copyright { get; set; }
+
+        [JsonPropertyName("base_uri")]
+        public string BaseUri { get; set; }
+
+        [JsonPropertyName("cycle")]
+        public int Cycle { get; set; }
+
+        [JsonPropertyName("fec_candidate")]
+        public string FecCandidate { get; set; }
+
+        [JsonPropertyName("support_total")]
+        public double SupportTotal { get; set; }
+
+        [JsonPropertyName("oppose_total")]
+        public double OpposeTotal { get; set; }
+
+        [JsonPropertyName("offset")]
+        public object Offset { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<IndependentExpenditurePerCandidateResult> Results { get; set; }
+    }
+
+
+}
---
+++ Propublica.CampaignFinance.Api/ApiClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Propublica.CampaignFinance.Api.Contracts;
+
+namespace Propublica.CampaignFinance.Api
+{
+    public class ApiClient
+    {
+        private readonly string apiKey;
+
+        private const string apiBaseUrl = "https://api.propublica.org/campaign-finance/";
+        private const string independentExpenditurePerCandidateUrl = "v1/{0}/candidates/{1}/independent_expenditures";
+        private const string candidateSearchUrl = "v1/{0}/candidates/search?query={1}";
+        private const string candidateUrl = "v1/{0}/candidates/{1}";
+
+        public ApiClient(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public async Task<CandidateSearchResponse> SearchCandidates(string cycle, string query)
+        {
+            var url = string.Format(candidateSearchUrl, cycle, query);
+            var response = await GetData<CandidateSearchResponse>(url);
+            return response;
+        }
+
+        public async Task<CandidateResponse> GetCandidate(string cycle, string fecId)
+        {
+            var url = string.Format(candidateUrl, cycle, fecId);
+            var response = await GetData<CandidateResponse>(url);
+            return response;
+        }
+
+        public async Task<IndependentExpenditurePerCandidateResponse> GetindependentExpenditurePerCandidate(string cycle, string fecId)
+        {
+            var url = string.Format(independentExpenditurePerCandidateUrl, cycle, fecId);
+            var response = await GetData<IndependentExpenditurePerCandidateResponse>(url);
+            return response;
+        }
+
+
+        private async Task<T> GetData<T>(string relativeurl, string baseUrl = apiBaseUrl)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("X-API-Key", apiKey);
+            var json = await httpClient.GetStringAsync(baseUrl + relativeurl);
+            var result = JsonSerializer.Deserialize<T>(json);
+            return result;
+        }
+    }
+}
