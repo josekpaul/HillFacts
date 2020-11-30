6a8f2b95facf912e779f31f715791440f6a6b753
Jose Paul <jose.k.paul@gmail.com>
2020-11-30 08:27:09 -0500

    Added stacked column chart

--- HfTimeline/wwwroot/d3VisualizationFunctions.js
+++ HfTimeline/wwwroot/d3VisualizationFunctions.js
@@ -1,10 +1,23 @@
 ﻿window.d3VisualizationFunctions = {
 	drawTreemap: function (data, elem) {
-		// set the dimensions and margins of the graph
+		var stringToColour = function (str) {
+			var hash = 0;
+			for (var i = 0; i < str.length; i++) {
+				hash = str.charCodeAt(i) + ((hash << 5) - hash);
+			}
+			var colour = '#';
+			for (var i = 0; i < 3; i++) {
+				var value = (hash >> (i * 8)) & 0xFF;
+				colour += ('00' + value.toString(16)).substr(-2);
+			}
+			return colour;
+		}
+
+		d3Selection = d3.select(elem);
+		var w = d3Selection.node().getBoundingClientRect().width;
 		var margin = { top: 10, right: 10, bottom: 10, left: 10 },
-			width = 445 - margin.left - margin.right,
-			height = 445 - margin.top - margin.bottom;
-		d3Selection = elem? d3.select(elem): d3.select(".treemapContainer");
+			width = w,
+			height = w;
 		// append the svg object to the body of the page
 		var svg = d3Selection
 			.append("svg")
@@ -14,17 +27,28 @@
 			.attr("transform",
 				"translate(" + margin.left + "," + margin.top + ")");
 
-//		var data = { "children": [{ "name": "boss1", "children": [{ "name": "mister_a", "group": "A", "value": 28, "colname": "level3" }, { "name": "mister_b", "group": "A", "value": 19, "colname": "level3" }, { "name": "mister_c", "group": "C", "value": 18, "colname": "level3" }, { "name": "mister_d", "group": "C", "value": 19, "colname": "level3" }], "colname": "level2" }, { "name": "boss2", "children": [{ "name": "mister_e", "group": "C", "value": 14, "colname": "level3" }, { "name": "mister_f", "group": "A", "value": 11, "colname": "level3" }, { "name": "mister_g", "group": "B", "value": 15, "colname": "level3" }, { "name": "mister_h", "group": "B", "value": 16, "colname": "level3" }], "colname": "level2" }, { "name": "boss3", "children": [{ "name": "mister_i", "group": "B", "value": 10, "colname": "level3" }, { "name": "mister_j", "group": "A", "value": 13, "colname": "level3" }, { "name": "mister_k", "group": "A", "value": 13, "colname": "level3" }, { "name": "mister_l", "group": "D", "value": 25, "colname": "level3" }, { "name": "mister_m", "group": "D", "value": 16, "colname": "level3" }, { "name": "mister_n", "group": "D", "value": 28, "colname": "level3" }], "colname": "level2" }], "name": "CEO" };
-
-
 		// Give the data to this cluster layout:
-		var root = d3.hierarchy(data).sum(function (d) { return d.value }); // Here the size of each leave is given in the 'value' field in input data
+		var root = d3.hierarchy(data).sum(function (d) { return d.value }) // Here the size of each leave is given in the 'value' field in input data
 
 		// Then d3.treemap computes the position of each element of the hierarchy
 		d3.treemap()
 			.size([width, height])
-			.padding(2)
-			(root);
+			.paddingTop(28)
+			.paddingRight(7)
+			.paddingInner(3)      // Padding between each rectangle
+			//.paddingOuter(6)
+			//.padding(20)
+			(root)
+
+		// prepare a color scale
+		var color = d3.scaleOrdinal()
+			.domain(["boss1", "boss2", "boss3"])
+			.range(["#402D54", "#D18975", "#8FD175"])
+
+		// And a opacity scale
+		var opacity = d3.scaleLinear()
+			.domain([10, 30])
+			.range([.5, 1])
 
 		// use this information to add rectangles:
 		svg
@@ -37,7 +61,8 @@
 			.attr('width', function (d) { return d.x1 - d.x0; })
 			.attr('height', function (d) { return d.y1 - d.y0; })
 			.style("stroke", "black")
-			.style("fill", "slateblue");
+			.style("fill", function (d) { return stringToColour(d.parent.data.name) })
+			.style("opacity", function (d) { return opacity(d.data.value) })
 
 		// and to add the text labels
 		svg
@@ -47,11 +72,101 @@
 			.append("text")
 			.attr("x", function (d) { return d.x0 + 5 })    // +10 to adjust position (more right)
 			.attr("y", function (d) { return d.y0 + 20 })    // +20 to adjust position (lower)
-			.text(function (d) { return d.data.name })
-			.attr("font-size", "15px")
+			.text(function (d) { return (d.data.name) })
+			.attr("font-size", "19px")
 			.attr("fill", "white")
-	}
+
+		// and to add the text labels
+		svg
+			.selectAll("vals")
+			.data(root.leaves())
+			.enter()
+			.append("text")
+			.attr("x", function (d) { return d.x0 + 5 })    // +10 to adjust position (more right)
+			.attr("y", function (d) { return d.y0 + 35 })    // +20 to adjust position (lower)
+			.text(function (d) { return d.data.value })
+			.attr("font-size", "11px")
+			.attr("fill", "black")
+
+		// Add title for the 3 groups
+		svg
+			.selectAll("titles")
+			.data(root.descendants().filter(function (d) { return d.depth == 1 }))
+			.enter()
+			.append("text")
+			.attr("x", function (d) { return d.x0 })
+			.attr("y", function (d) { return d.y0 + 21 })
+			.text(function (d) { return d.data.name })
+			.attr("font-size", "19px")
+			.attr("fill", function (d) { return stringToColour(d.data.name) })
+
+		// Add title for the 3 groups
+		svg
+			.append("text")
+			.attr("x", 0)
+			.attr("y", 14)    // +20 to adjust position (lower)
+			.text("Three group leaders and 14 employees")
+			.attr("font-size", "19px")
+			.attr("fill", "grey")
+	},
+
+	drawStackedColumnChart: function (dataTable, categoryColumn, elem) {
+
+		d3Selection = d3.select(elem);
+		var w = 440;// d3Selection.node().getBoundingClientRect().width;
+
+		// set the dimensions and margins of the graph
+		var margin = { top: 10, right: 30, bottom: 20, left: 50 },
+			width = w - margin.left - margin.right,
+			height = Math.ceil(w / 1.64) - margin.top - margin.bottom;
+
+		// append the svg object to the body of the page
+
+		var data = d3.csvParse(dataTable, d3.autoType);
+
+		var series = d3.stack().keys(data.columns.slice(1))(data);
+
+		const svg = d3Selection
+			.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom);
+
+		const gSvg = svg.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		const xScale = d3.scaleBand()
+			.domain(data.map(function (d) { return d[categoryColumn]; }))
+			.range([0, width])
+			.padding(0.1);
+
+		const yScale = d3.scaleLinear()
+			.domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+			.range([height, 0]);
+
+		const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+		const rects = gSvg.selectAll("g").data(series).enter()
+			.append("g")
+			.attr("fill", d => color(d.key)); //Color is assigned here because you want everyone for the series to be the same color
+
+		rects.selectAll("rect")
+			.data(d => d)
+			.join("rect")
+			.attr("x", (d, i) => xScale(d.data[categoryColumn]))
+			.attr("y", d => yScale(d[1]))
+			.attr("height", d => yScale(d[0]) - yScale(d[1]))
+			.attr("width", xScale.bandwidth())
+    /*.on("mouseover", function(){d3.select(this).attr("fill", "purple")})
+    .on("mouseout", function(){d3.select(this).attr("fill", color(series.key))})*/;
+
+		const xAxis = gSvg.append("g")
+			.attr("id", "xAxis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(xScale));
+
+		const yAxis = gSvg.append("g")
+			.attr("id", "yAxis")
+			.call(d3.axisLeft(yScale));
+  }
 }
-
-
 
--- HillFacts/Client/ViewModels/CampFiCandidateViewModel.cs
+++ HillFacts/Client/ViewModels/CampFiCandidateViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using D3Visualizations.Services;
 using HillFacts.Client.Services;
@@ -60,15 +61,15 @@ namespace HillFacts.Client.ViewModels
             if (independentExpenditure.Results != null)
             {
                 var treemap = new TreemapInput { Name = "Independent Expenditures"};
-                treemap.Children = independentExpenditure.Results.Select(r => new TreemapInput
+                treemap.Children = independentExpenditure.Results.GroupBy(r => r.CandidateName).Select(r => new TreemapInput
                 {
-                    Name = r.CandidateName,
+                    Name = r.Key,
                     Children = new List<TreemapInput> { new TreemapInput { Name = "Support" , Children = new List<TreemapInput>()},
                         new TreemapInput { Name = "Oppose"  , Children = new List<TreemapInput>()} }.ToList()
                 }).ToList();
                 foreach(var result in independentExpenditure.Results)
                 {
-                    if (int.TryParse(result.Amount, out int amt))
+                    if (double.TryParse(result.Amount, out double amt))
                     {
                         if (result.SupportOrOppose == "S")
                         {
@@ -79,8 +80,15 @@ namespace HillFacts.Client.ViewModels
                             treemap.Children.Where(c => c.Name == result.CandidateName).First().Children[1].Children.Add(new TreemapInput { Name = result.Payee, Value = amt });
                         }
                     }
+                    else
+                    {
+                        string r = result.Amount;
+                    }
                 }
-               ExpenditureTreemapped = treemap;
+                var x = JsonSerializer.Serialize(treemap, new JsonSerializerOptions { WriteIndented = true });
+                ExpenditureTreemapped = treemap;
+                //ExpenditureTreemapped = JsonSerializer.Deserialize<TreemapInput>(@"{ ""Children"": [{ ""Name"": ""boss1"", ""Children"": [{ ""Name"": ""mister_a"", ""Group"": ""A"", ""Value"": 28, ""ColName"": ""level3"" }, { ""Name"": ""mister_b"", ""Group"": ""A"", ""Value"": 19, ""ColName"": ""level3"" }, { ""Name"": ""mister_c"", ""Group"": ""C"", ""Value"": 18, ""ColName"": ""level3"" }, { ""Name"": ""mister_d"", ""Group"": ""C"", ""Value"": 19, ""ColName"": ""level3"" }], ""ColName"": ""level2"" }, { ""Name"": ""boss2"", ""Children"": [{ ""Name"": ""mister_e"", ""Group"": ""C"", ""Value"": 14, ""ColName"": ""level3"" }, { ""Name"": ""mister_f"", ""Group"": ""A"", ""Value"": 11, ""ColName"": ""level3"" }, { ""Name"": ""mister_g"", ""Group"": ""B"", ""Value"": 15, ""ColName"": ""level3"" }, { ""Name"": ""mister_h"", ""Group"": ""B"", ""Value"": 16, ""ColName"": ""level3"" }], ""ColName"": ""level2"" }, { ""Name"": ""boss3"", ""Children"": [{ ""Name"": ""mister_i"", ""Group"": ""B"", ""Value"": 10, ""ColName"": ""level3"" }, { ""Name"": ""mister_j"", ""Group"": ""A"", ""Value"": 13, ""ColName"": ""level3"" }, { ""Name"": ""mister_k"", ""Group"": ""A"", ""Value"": 13, ""ColName"": ""level3"" }, { ""Name"": ""mister_l"", ""Group"": ""D"", ""Value"": 25, ""ColName"": ""level3"" }, { ""Name"": ""mister_m"", ""Group"": ""D"", ""Value"": 16, ""ColName"": ""level3"" }, { ""Name"": ""mister_n"", ""Group"": ""D"", ""Value"": 28, ""ColName"": ""level3"" }], ""ColName"": ""level2"" }], ""Name"": ""CEO"" }");
+
             }
         }
 
